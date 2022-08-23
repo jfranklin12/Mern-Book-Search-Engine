eeb4e00b2973f3b34c105a3d363966a035de3107
jfranklin12 <j.franks725@gmail.com>
2022-08-23 18:00:22 -0400

    intial server set up with schema, auth and serverjs

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,35 @@
+const { Tech, Matchup } = require('../models');
+
+const resolvers = {
+  Query: {
+    tech: async () => {
+      return Tech.find({});
+    },
+    matchups: async (parent, { _id }) => {
+      const params = _id ? { _id } : {};
+      return Matchup.find(params);
+    },
+  },
+  Mutation: {
+    createMatchup: async (parent, args, context) => {
+      if(context.user) {
+
+      
+      const matchup = await Matchup.create(args);
+      return matchup;
+      }
+      // you still need to import appolo
+      throw new AuthenticationError('There was problem with login');
+    },
+    createVote: async (parent, { _id, techNum }) => {
+      const vote = await Matchup.findOneAndUpdate(
+        { _id },
+        { $inc: { [`tech${techNum}_votes`]: 1 } },
+        { new: true }
+      );
+      return vote;
+    },
+  },
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,7 @@
+const { gql } = require('apollo-server-express');
+
+// use readme and miniproject to set typeDefs
+const typeDefs = gql`
+`;
+
+module.exports = typeDefs;
