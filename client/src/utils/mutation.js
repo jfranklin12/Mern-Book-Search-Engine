1c9f59202ed928ad01d7e7b10717cf527f2f5ed3
jfranklin12 <j.franks725@gmail.com>
2022-08-25 16:13:18 -0400

    mutation update

--- client/src/utils/mutation.js
+++ client/src/utils/mutation.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-  mutation loginUser($_id: ID!, username: String!, email: String) {
+  mutation loginUser($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
       user {
